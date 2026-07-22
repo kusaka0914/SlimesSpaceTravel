a47e3f105858eb5c3a4de03a549f6554883aed9d
marimo117 <153973336+marimo117@users.noreply.github.com>
2026-07-23 03:36:55 +0900

    windowsで重くなる問題を修正しました

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,17 @@
 #include "Game.h"
 #include <cstring>
 
+#ifdef _WIN32
+
+extern "C" {
+
+__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+#endif
+
 bool HandleDebugCommand(int argc, const char* argv[])
 {
     for (int i = 1; i < argc; ++i) {
